295d954336ca311aa61767d27f4a7af436bc1585
Fahad_Ahmed <fahad@mailbox.unideb.hu>
2025-09-04 02:25:13 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, Title } from "@mantine/core";
 import { theme } from "./theme";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <Title order={1}>Testing deployment!</Title>
+    </MantineProvider>
+  );
 }
